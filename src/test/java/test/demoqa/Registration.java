196ae384ddeae5f35253491b291e879854b2507c
Anton Smerdov <anton.smerdov95@yandex.ru>
2022-04-11 21:45:44 +0300

    addad class

---
+++ src/test/java/test/demoqa/Registration.java
@@ -0,0 +1,43 @@
+package test.demoqa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class Registration {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE_NUMBER = 68;
+
+    @Test
+    public void testIssueSearch() {
+
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("открываем главную страницу", () -> {
+            open("https://github.com");
+        });
+
+        step("ищем репозиторий" + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("открываем репозиторий" + REPOSITORY, () -> {
+            $(By.linkText("eroshenkoam/allure-example")).click();
+        });
+        step("переходим в таб Issues", () -> {
+            $(By.partialLinkText("Issues")).click();
+        });
+        step("проверка что существует Issues с номером" + ISSUE_NUMBER, () -> {
+            $(withText(String.valueOf(ISSUE_NUMBER))).should(Condition.exist);
+        });
+
+    }
+}
--- src/test/java/test/simple/NegativeTests.java
+++ src/test/java/test/simple/NegativeTests.java
@@ -1,4 +1,4 @@
-package test;
+package test.simple;
 
 import org.junit.jupiter.api.Test;
 
